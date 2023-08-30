3109d72d41866126d415b1ddcc92eb1927c841bf
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-08-30 17:32:23 +0200

    Complete horizontal positioning and test

--- src/logic/players.js
+++ src/logic/players.js
@@ -29,9 +29,38 @@ export class Player {
 
   placeShip(x, y) {
     const ship = this.ships[this.shipsPlacedCount];
-    this.board[x][y].ship = ship;
+    const shipSize = ship.size;
+
+    //Check ship won't be out of limit
+    if (shipSize > this.board[x].length - 1) return null;
+    //Check no other ships around
+    else {
+      //Check left
+      if (y > 0) {
+        if (this.board[x][y - 1].ship) return null;
+      }
+      for (let i = 0; i < shipSize; i++) {
+        //Check Self
+        if (this.board[x][y + i].ship) return null;
+        //Check Bottom
+        if (x < 9) {
+          if (this.board[x + 1][y + i].ship) return null;
+        }
+        //Check top
+        if (x > 0) {
+          if (this.board[x - 1][y + i].ship) return null;
+        }
+      }
+      if (this.board[x][y + shipSize - 1].ship) return null; //Right
+    }
+
+    //Place ship
+    for (let i = 0; i < shipSize; i++) {
+      this.board[x][y + i].ship = ship;
+    }
 
     this.shipsPlacedCount += 1;
+    return `${ship.name} placed`;
   }
 }
 
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -14,3 +14,32 @@ it('Player: Place a ship', () => {
   PlayerExample.placeShip(0, 0);
   expect(PlayerExample.board[0][0].ship.name).toBe('Carrier');
 });
+
+it('Player: Ship properly placed', () => {
+  expect(PlayerExample.board[0][4].ship.name).toBe('Carrier');
+});
+
+it("Player: Can't place if a ship is on the way", () => {
+  expect(PlayerExample.placeShip(0, 2)).toBe(null);
+});
+
+it("Player: Can't place if a ship is on top", () => {
+  expect(PlayerExample.placeShip(1, 2)).toBe(null);
+});
+
+it("Player: Can't place if a ship is right behind", () => {
+  expect(PlayerExample.placeShip(0, 5)).toBe(null);
+});
+
+it('Player: Can place on the very next one', () => {
+  expect(PlayerExample.placeShip(0, 6)).toBe('Battleship placed');
+});
+
+it("Player: Can't place if a ship is underneath", () => {
+  PlayerExample.placeShip(9, 6);
+  expect(PlayerExample.placeShip(8, 6)).toBe(null);
+});
+
+it("Player: Can't place if a ship is in front", () => {
+  expect(PlayerExample.placeShip(9, 3)).toBe(null);
+});
